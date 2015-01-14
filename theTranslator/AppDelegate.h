f4043d5621d8ad826914a9b75f99f8a0e46c2be6
Jan Roures Mintenig <janroures@iMac-de-Jan.local>
2015-01-14 11:21:55 +0100

    Initial Commit

---
+++ theTranslator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  theTranslator
+//
+//  Created by Jan Roures Mintenig on 14/1/15.
+//  Copyright (c) 2015 Jan Roures Mintenig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ theTranslator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  theTranslator
+//
+//  Created by Jan Roures Mintenig on 14/1/15.
+//  Copyright (c) 2015 Jan Roures Mintenig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
